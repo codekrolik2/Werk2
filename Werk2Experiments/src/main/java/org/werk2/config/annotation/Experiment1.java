c78ba8a259b337e423f063462750098f2d66258d
jamirov <jamirov@jamirov-ThinkPad-X1-Carbon-6th>
2021-05-08 20:46:16 -0700

    Project for local experiments added

---
+++ Werk2Experiments/src/main/java/org/werk2/config/annotation/Experiment1.java
@@ -0,0 +1,16 @@
+package org.werk2.config.annotation;
+
+import java.util.List;
+
+import org.werk2.config.annotation.scan.WerkAnnotationScanner;
+import org.werk2.config.functions.Function;
+
+public class Experiment1 {
+	public static void main(String[] args) throws Exception {
+		List<Function> functions = new WerkAnnotationScanner().loadRawFunctions();
+		for (Function f : functions) {
+			System.out.println(f);
+			System.out.println();
+		}
+	}
+}
---
+++ Werk2Experiments/src/main/java/org/werk2/config/annotation/TestFuncChild.java
@@ -0,0 +1,24 @@
+package org.werk2.config.annotation;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.werk2.common.OutParam;
+import org.werk2.config.annotation.annotations.In;
+import org.werk2.config.annotation.annotations.Out;
+import org.werk2.config.annotation.annotations.WerkFunction;
+
+public class TestFuncChild extends TestFunc {
+	@WerkFunction
+	public static int func1(@In int i, @In char c, @In ArrayList<Boolean> lst1, @Out OutParam<Map<String, List<Integer>>> mapx) {
+		System.out.println(i + " : " + c);
+		return -100;
+	}
+
+	@WerkFunction
+	public static Integer func2(@In int a, @In Integer b, @In String c, @In InputStream d, @Out OutParam<String> out1, @Out OutParam<InputStream> out2) {
+		return -100;
+	}
+}
